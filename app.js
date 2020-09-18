accce78650b00e6b3c7802003e560a112a800e9c
WDoyle21 <57500204+WDoyle21@users.noreply.github.com>
2020-09-18 14:47:14 -0400

    9/18

---
+++ app.js
@@ -0,0 +1,18 @@
+var life = 100; // Comment (Life Bar)
+
+life = life - 10;
+//90
+life = life - 10;
+//80
+
+var name = 'Will'; // Display Name
+
+var checkout = false; //boolean
+
+var box; //undefined variable
+
+const lives = 3; //Can't be modified (constant)
+
+//'let' can be used to create a variable that can be changed
+
+console.log(box);

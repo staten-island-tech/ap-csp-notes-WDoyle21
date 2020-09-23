4c90bc72d63b77b9f74ff2b512fb9b41f58f4564
WDoyle21 <57500204+WDoyle21@users.noreply.github.com>
2020-09-23 19:54:15 -0400

    9/23

--- app.js
+++ app.js
@@ -1,18 +1,83 @@
-var life = 100; // Comment (Life Bar)
+//Strings:
+const name1 = "Will";
 
-life = life - 10;
-//90
-life = life - 10;
-//80
+const myAge = 16;
 
-var name = 'Will'; // Display Name
+const yourAge = 24;
 
-var checkout = false; //boolean
+console.log("Hello my name is "+ "Ed");
 
-var box; //undefined variable
+console.log(`Hello it's me ${name1} and I am ${myAge} years old.`);
 
-const lives = 3; //Can't be modified (constant)
+console.log(typeof yourAge);
 
-//'let' can be used to create a variable that can be changed
+//Functions:
+const name2 = "William";
 
-console.log(box);
+const youtuber = "Traversy Media";
+
+function toUpper(text) {
+ const upperCased = text.toUpperCase();
+ console.log(upperCased);
+}
+
+const toUpper = () => {
+
+}
+
+//Math:
+const num1 = 100;
+const num2 = 50;
+let val;
+
+// Simple Math //
+val = num1 + num2;
+val = num1 - num2;
+val = num1 * num2;
+val = num1 / num2;
+val = num1 % num2;
+
+// Math Object //
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.4);
+val = Math.ceil(2.4);
+val = Math.floor(2.8);
+val = Math.sqrt(64);
+val = Math.abs(-3);
+val = Math.pow(8, 2);
+val = Math.min(1,2);
+val = Math.max(3,4);
+val = Math.random();
+
+console.log(val);
+
+//Parameters and Arguments//
+function calculateBill(billAmount, taxRate = 0.13, tipRate = 0.15);
+{
+    //function body//
+    console.log('Running Calculate Bill!');
+    const total = billAmount + (billAmount * taxRate + billAmount * tipRate);
+    return total;
+}
+
+//function call//
+const wesTotal = 500;
+const wesTaxRate = 0.3;
+
+//function definition//
+function sayHiTo(firstName) {
+    return `Hello ${firstName}`
+}
+
+function doctorize(name){
+    return `Dr. ${name}`;
+}
+
+function yell(name = 'Silly Goose') {
+    return `HEY ${name.toUpperCase()}`;
+}
+
+const myBill4 = calculateBill(100, undefined ,0.2);
+console.log(myBill4);
+

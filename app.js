dd47e4d0d0aa1d28354feba69de975d305af6dfd
WDoyle21 <57500204+WDoyle21@users.noreply.github.com>
2020-09-29 21:41:05 -0400

    9/29

--- app.js
+++ app.js
@@ -1,18 +1,90 @@
-var life = 100; // Comment (Life Bar)
+// If/Else:
 
-life = life - 10;
-//90
-life = life - 10;
-//80
+const dice1 = 6;
+const dice2 = 6;
 
-var name = 'Will'; // Display Name
+if (dice1 === 6 && dice2 === 6) {
+    console.log("You rolled a double");
+} else {
+console.log("You didn't");
+}
 
-var checkout = false; //boolean
+// Objects:
 
-var box; //undefined variable
+const user = {
+    name: "Will",
+    age: 24,
+    married: false,
+    purchases: ["phone", "car", "laptop"],
 
-const lives = 3; //Can't be modified (constant)
+    sayName: function() {
+        console.log(this.name);
+    }
+}
 
-//'let' can be used to create a variable that can be changed
+user.sayName();
 
-console.log(box);
+function saymyAge(){
+    console.log(`My age is ${this}`);
+}
+
+saymyAge();
+
+// Switches:
+
+const color = 'yellow';
+
+switch(color){
+    case 'red':
+        console.log('Color is red');
+        break;
+    case 'blue':
+        console.log('Color is blue');
+         break;
+    default:
+        console.log('Color is not red or blue');
+        break;
+}
+
+switch(new Date().getDay()){
+    case 0:
+        day = 'Sunday';
+        break;
+    case 1:
+        day = 'Monday';
+        break;
+    case 2:
+        day = 'Tuesday';
+        break;
+    case 3:
+        day = 'Wednesday';
+        break;
+    case 4:
+        day = 'Thursday';
+        break;
+    case 5:
+        day = 'Friday';
+        break;
+    case 6:
+        day = 'Saturday';
+        break;
+}
+
+console.log('Today is ${day}');
+
+// Traversy Arrays
+
+const numbers = [1,2,3,4,5,6,7];
+const numbers2 = new Array(8,9,10,11,12);
+const fruit = ['Apple', 'Banana', 'Orange', 'Pear'];
+const mixed = [1, 'Hello', true, undefined, null, {a:1, b:1}, new Date()];
+
+let val;
+
+val = numbers.length;
+val = Array.isArray(numbers);
+val = numbers[3];
+numbers[2] = 100;
+
+console.log(numbers);
+console.log(val);
